300630b0a66769d6df8039c752bce283165bfddc
JeremyMcClelland <jeremy@moraycreative.com>
2017-11-06 18:15:20 -0500

    Minesweeper Day 21 Complete
    move game and board into modules and practice import export

--- instructions.js
+++ instructions.js
@@ -486,3 +486,81 @@ export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRan
 }
 
 
+
+//Airplane import////////////////////////////////////
+import {availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+function displaySpeedRangeStatus(){
+  availableAirplanes.forEach(function(element){
+    console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+    
+  });
+    
+}
+
+displayStaffStatus();
+
+displaySpeedRangeStatus();
+///////////////////////////////////////////////////
+
+export let availableAirplanes = [{
+ name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+  maxSpeed: 1200,
+  minSpeed: 300
+}, 
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}];
+
+export let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
+};
+
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange){
+  let range = maxSpeed - minSpeed;
+  
+   if (range > requiredSpeedRange) {
+    return true;
+    } else {
+    return false;
+  }
+}
+
+
+//Export as new name
+
+let specialty = '';
+let isVegetarian = function() {
+}; 
+let isLowSodium = function() {
+}; 
+
+export { specialty as chefsSpecial, isVegetarian as isVeg, isLowSodium };
